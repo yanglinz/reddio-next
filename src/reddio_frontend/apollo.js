f7d138ae6a52841e1c77de36ca2957af442e15fa
Yanglin Zhao <yz1876@gmail.com>
2017-02-09 22:58:34 -0500

    Setup apollo client

---
+++ src/reddio_frontend/apollo.js
@@ -0,0 +1,18 @@
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({
+    uri: 'http://graphql.org/swapi-graphql/'
+  })
+});
+
+function RootProvider(props) {
+  return (
+    <ApolloProvider client={client}>
+      {props.children}
+    </ApolloProvider>
+  );
+}
+
+export default RootProvider;
--- src/reddio_frontend/entry.js
+++ src/reddio_frontend/entry.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import RootProvider from './apollo';
+
 window.React = React;
 window.ReactDOM = ReactDOM;
+
+window.modules = {
+  RootProvider
+};
