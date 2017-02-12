6fbc1292727110503a020a2281b9250bad4fc536
Yanglin Zhao <yz1876@gmail.com>
2017-02-12 18:12:00 -0500

    Exclude react and react-dom from cljs build

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                  [org.clojure/clojurescript "1.9.229"]
                  [org.clojure/core.async "0.2.395"
                   :exclusions [org.clojure/tools.reader]]
-                 [reagent "0.6.0"]]
+                 [reagent "0.6.0" :exclusions [cljs/react cljs/react-dom]]]
 
   :plugins [[lein-figwheel "0.5.9"]
             [lein-cljfmt "0.5.6"]
