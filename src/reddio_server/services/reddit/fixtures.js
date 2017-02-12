0e57c59e275a2289657041a7cdb698dca97fe846
Yanglin Zhao <yz1876@gmail.com>
2017-02-12 14:53:49 -0500

    Add popular subreddits

--- src/reddio_server/services/reddit/fixtures.js
+++ src/reddio_server/services/reddit/fixtures.js
@@ -1,8 +1,33 @@
 const SUBREDDITS = [
   {
     id: '/r/listentothis',
-    displayName: 'listentothis',
     urlPath: '/r/listentothis',
+    displayName: 'listentothis'
+  },
+  {
+    id: '/r/hiphopheads',
+    urlPath: '/r/hiphopheads',
+    displayName: 'hiphopheads'
+  },
+  {
+    id: '/r/electronicmusic',
+    urlPath: '/r/electronicmusic',
+    displayName: 'electronicmusic'
+  },
+  {
+    id: '/r/metal',
+    urlPath: '/r/metal',
+    displayName: 'metal'
+  },
+  {
+    id: '/r/dubstep',
+    urlPath: '/r/dubstep',
+    displayName: 'dubstep'
+  },
+  {
+    id: '/r/mashups',
+    urlPath: '/r/mashups',
+    displayName: 'mashups'
   }
 ];
 
