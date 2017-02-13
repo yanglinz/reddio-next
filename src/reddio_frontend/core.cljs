284375a045ee1fcb17a86f7740a8c5faaf58d409
Yanglin Zhao <yz1876@gmail.com>
2017-02-12 23:00:09 -0500

    Remove boilerplate comments in core.cljs

--- src/reddio_frontend/core.cljs
+++ src/reddio_frontend/core.cljs
@@ -2,26 +2,13 @@
   (:require [reagent.core :as reagent :refer [atom]]
             [reddio-frontend.bridge :as bridge]))
 
-(enable-console-print!)
-
-(println "This text is printed from src/reddio-next/core.cljs. Go ahead and edit it and see reloading in action.")
-
-;; define your app data so that it doesn't get over-written on reload
-
-(defonce app-state (atom {:text "Hello world!"}))
-
 (defn hello-world []
   [:div
-   [:h1 (:text @app-state)]
-   [:h2 "Another one!"]
+   [:h1 "Hello world!"]
    [:> bridge/root-provider
     [:> bridge/home-top-subreddits]]])
 
 (reagent/render-component [hello-world]
                           (. js/document (getElementById "app")))
 
-(defn on-js-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+(defn on-js-reload [])
