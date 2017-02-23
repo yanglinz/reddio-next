3aef3747a41f22af0162fbf669469d23fb87de07
Yanglin Zhao <yz1876@gmail.com>
2017-02-22 21:15:22 -0500

    Rename query enhancers

--- src/reddio_frontend/bridge.js
+++ src/reddio_frontend/bridge.js
@@ -1,6 +1,6 @@
 import RootProvider from './components/apollo';
-import enhanceHomeTopSubredditQuery from './screens/home/top-subreddits';
-import enhanceListingsQuery from './screens/listings/listings';
+import enhanceHomeTopSubredditQuery from './screens/home/top-subreddits-query';
+import enhanceListingsQuery from './screens/listings/listings-query';
 
 window.modules = {
   RootProvider,
