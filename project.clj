339eb09d4ac8ecbb9d373b8134ac5f097db33633
Yanglin Zhao <yz1876@gmail.com>
2017-02-22 20:48:14 -0500

    Open url without index.html

--- project.clj
+++ project.clj
@@ -38,7 +38,7 @@
                            ;; in the default browser once Figwheel has
                            ;; started and complied your application.
                            ;; Comment this out once it no longer serves you.
-                           :open-urls ["http://localhost:3449/index.html"]}
+                           :open-urls ["http://localhost:3449"]}
 
                 :compiler {:main reddio-frontend.core
                            :asset-path "/js/compiled/out"
