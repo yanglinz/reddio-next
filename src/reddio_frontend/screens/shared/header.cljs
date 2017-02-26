be5066a142b5ceb99c3af4d8194bfa22845916cd
Yanglin Zhao <yz1876@gmail.com>
2017-02-26 10:15:27 -0500

    Add header grid

--- src/reddio_frontend/screens/shared/header.cljs
+++ src/reddio_frontend/screens/shared/header.cljs
@@ -1,6 +1,15 @@
 (ns reddio-frontend.screens.shared.header
   (:require))
 
-  [:div.header
 (defn main []
-   [:h1 "Header"]])
+  [:div.header.container-fluid
+   [:div.row
+    [:div.col-sm-3
+     [:div.header-brand
+      [:h1 "Reddio"]]]
+    [:div.col-sm-6
+     [:div.header-search
+      [:h1 "Search"]]]
+    [:div.col-sm-3
+     [:div.header-nav
+      [:h1 "Navigation"]]]]])
