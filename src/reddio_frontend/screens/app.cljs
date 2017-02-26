ed8db32e381158133bbdcbc45c011dec93a57401
Yanglin Zhao <yz1876@gmail.com>
2017-02-25 22:56:13 -0500

    Add header.cljs

--- src/reddio_frontend/screens/app.cljs
+++ src/reddio_frontend/screens/app.cljs
@@ -1,8 +1,10 @@
 (ns reddio-frontend.screens.app
   (:require [reddio-frontend.bridge :as bridge]
-            [reddio-frontend.screens.listings.listings :as listings]))
+            [reddio-frontend.screens.shared.header :refer [header]]
+            [reddio-frontend.screens.listings.listings :refer [listings]]))
 
 (defn app []
   [:> bridge/root-provider
    [:div.app
-    [listings/listings {:url-path "/r/listentothis"}]]])
+    [header]
+    [listings {:url-path "/r/listentothis"}]]])
---
+++ src/reddio_frontend/screens/shared/header.cljs
@@ -0,0 +1,6 @@
+(ns reddio-frontend.screens.shared.header
+  (:require))
+
+(defn header []
+  [:div.header
+   [:h1 "Header"]])
