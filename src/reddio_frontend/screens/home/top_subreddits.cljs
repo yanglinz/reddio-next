1cb037de35141440415244fc4927b6379729c9e1
Yanglin Zhao <yz1876@gmail.com>
2017-02-27 17:55:51 -0500

    Display a card for each of the top subreddits

--- src/reddio_frontend/screens/home/top_subreddits.cljs
+++ src/reddio_frontend/screens/home/top_subreddits.cljs
@@ -3,9 +3,18 @@
             [reddio-frontend.utilities.core :as u]
             [reddio-frontend.bridge :as bridge]))
 
+(defn subreddit-card [subreddit]
+  [:div.subreddit-card
+   [:h3 (:url-path subreddit)]])
+
 (defn top-subreddits [data]
-  [:div.top-subreddits
-   [:h1 "Top Subreddits"]])
+  (let [top-subreddits (:top-subreddits data)]
+    [:div.top-subreddits.container-fluid
+     [:div.row
+      (for [subreddit top-subreddits]
+        ^{:key (:id subreddit)}
+        [:div.col-sm-4
+         [subreddit-card subreddit]])]]))
 
 (defn top-subreddits-container [apollo-props]
   (let [props (u/kebab-case-keywordize-keys (js->clj apollo-props))
