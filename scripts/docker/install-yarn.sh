cc100deb2a97c40a6e64c1649d68e4612d6fc869
Yanglin Zhao <yz1876@gmail.com>
2018-12-21 22:49:19 -0500

    Remove legacy files

--- scripts/docker/install-yarn.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euox pipefail
-IFS=$'\n\t'
-
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-apt-get update && apt-get install yarn
