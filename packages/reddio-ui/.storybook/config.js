73fbbbfae4d254be531a673cad9dd76f7b60855a
Yanglin Zhao <yz1876@gmail.com>
2018-12-22 21:09:36 -0500

    Add centered decorator

--- packages/reddio-ui/.storybook/config.js
+++ packages/reddio-ui/.storybook/config.js
@@ -1,4 +1,7 @@
-import { configure } from "@storybook/react";
+import { configure, addDecorator } from "@storybook/react";
 import { loadStories } from "./storyLoader";
+import { centeredDecorator } from "./decorators";
+
+addDecorator(centeredDecorator);
 
 configure(loadStories, module);
