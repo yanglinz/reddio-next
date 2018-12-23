a37c59ccc9bdfd363cc35886f6fd643c1c68b893
Yanglin Zhao <yz1876@gmail.com>
2018-12-23 18:31:37 -0500

    Support image imports in storybook

--- packages/reddio-ui/.storybook/webpack.config.js
+++ packages/reddio-ui/.storybook/webpack.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  module: {
+    rules: [
+      {
+        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+        loader: "file-loader",
+      }
+    ]
+  },
   resolve: {
     alias: {
       "react-native$": "react-native-web"
