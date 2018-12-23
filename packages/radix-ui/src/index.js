e4a2bea9a0d890c8f69fb608f5ececaf28f274c2
Yanglin Zhao <yz1876@gmail.com>
2018-12-22 19:01:35 -0500

    Remove service worker import

--- packages/radix-ui/src/index.js
+++ packages/radix-ui/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as serviceWorker from "./serviceWorker";
 
 import AppRoutes from "./app/routes";
 
@@ -9,4 +8,5 @@ ReactDOM.render(<AppRoutes />, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// import * as serviceWorker from "./serviceWorker";
+// serviceWorker.unregister();
