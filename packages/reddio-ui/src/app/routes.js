326e4e807e618e0292f159c9f06abc6165c42c1e
Yanglin Zhao <yz1876@gmail.com>
2018-12-22 19:28:21 -0500

    Create dynamic loader component

--- packages/reddio-ui/src/app/routes.js
+++ packages/reddio-ui/src/app/routes.js
@@ -2,23 +2,7 @@ import React from "react";
 import { ActivityIndicator, View } from "react-native";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-class DynamicImport extends React.Component {
-  state = {
-    component: null
-  };
-
-  componentDidMount() {
-    this.props.load().then(component => {
-      this.setState(() => ({
-        component: component.default ? component.default : component
-      }));
-    });
-  }
-
-  render() {
-    return this.props.children(this.state.component);
-  }
-}
+import DynamicImport from "../components/DynamicImport";
 
 function Loading() {
   return (
