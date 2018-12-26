f447e735ca7dac96ff3ca3f312cbb832cd931a15
Yanglin Zhao <yz1876@gmail.com>
2018-12-25 20:57:43 -0500

    Add basic post styles

--- packages/reddio-ui/src/components/PostList.js
+++ packages/reddio-ui/src/components/PostList.js
@@ -1,25 +1,38 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Image, Text, TouchableOpacity, StyleSheet } from "react-native";
 
 export function Post(props) {
-  const {
-    author,
-    name,
-    numComments,
-    score,
-    thumbnail,
-    title,
-    url
-  } = props.post;
+  const { author, numComments, score, thumbnail, title, url } = props.post;
   return (
-    <View>
-      <Text>{title}</Text>
-    </View>
+    <TouchableOpacity
+      style={styles.post}
+      onPress={() => props.onPress(props.post)}
+    >
+      <Image source={thumbnail} />
+      <View>
+        <Text href={url}>{title}</Text>
+        <Text style={styles.postMeta}>
+          {score} points | Posted by {author} | {numComments} comments
+        </Text>
+      </View>
+    </TouchableOpacity>
   );
 }
 
 export function PostList(props) {
-  return props.posts.map(p => <Post key={p.name} post={p} />);
+  const onPress = () => console.log("onPress");
+  return props.posts.map(p => <Post key={p.name} post={p} onPress={onPress} />);
 }
+
+const styles = StyleSheet.create({
+  post: {
+    borderColor: "#DDD",
+    borderBottomWidth: 1,
+    padding: 16
+  },
+  postMeta: {
+    color: "#999"
+  }
+});
 
 export default PostList;
--- packages/reddio-ui/src/components/PostList.story.js
+++ packages/reddio-ui/src/components/PostList.story.js
@@ -4,6 +4,15 @@ import { action } from "@storybook/addon-actions";
 
 import { Post } from "./PostList";
 
+const fakePost = {
+  author: "u123",
+  numComments: 123,
+  score: 456,
+  thumbnail: undefined,
+  title: "This is some post!",
+  url: "http://reddit.com"
+};
+
 storiesOf("PostList", module).add("post", () => (
-  <Post onClick={action("clicked")} />
+  <Post onClick={action("clicked")} post={fakePost} />
 ));
