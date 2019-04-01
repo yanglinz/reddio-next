7418f6bdf2c28dcf150f01067d7403fd115af94e
Yanglin Zhao <yz1876@gmail.com>
2019-03-31 21:28:03 -0400

    Center placeholder buttons

--- packages/reddio-ui/src/components/Player.js
+++ packages/reddio-ui/src/components/Player.js
@@ -1,19 +1,36 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Button, StyleSheet } from "react-native";
 
 function Player() {
   return (
     <View style={styles.player}>
-      <Text>Hello</Text>
+      <View style={styles.controls}>
+        <View style={styles.controlButton}>
+          <Button onPress={() => console.log("play")} title="Play" />
+        </View>
+
+        <View style={styles.controlButton}>
+          <Button onPress={() => console.log("pause")} title="Pause" />
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   player: {
+    display: "flex",
+    alignItems: "center",
     borderColor: "#ddd",
     borderTopWidth: 1,
     padding: 16
+  },
+  controls: {
+    flexDirection: "row"
+  },
+  controlButton: {
+    marginLeft: 5,
+    marginRight: 5
   }
 });
 
