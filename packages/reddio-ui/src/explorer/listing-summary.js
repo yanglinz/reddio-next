b94d00d3726aea59a251b39ef6f0111b383f76f4
Yanglin Zhao <yz1876@gmail.com>
2019-04-07 21:09:26 -0400

    Display a list of images

--- packages/reddio-ui/src/explorer/listing-summary.js
+++ packages/reddio-ui/src/explorer/listing-summary.js
@@ -1,21 +1,42 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Image, Text, StyleSheet } from "react-native";
+import uniq from "lodash/uniq";
+import take from "lodash/take";
 
 function ListingSummary(props) {
-  const { pathname } = props;
+  const { pathname, posts } = props;
+  let images = posts.map(p => p.thumbnail);
+  images = images.filter(Boolean);
+  images = images.filter(i => i.includes("https://") || i.includes("http://"));
+  images = take(uniq(images), 5);
+
   return (
     <View style={styles.summary}>
       <Text>{pathname}</Text>
+      <View style={styles.imageReel}>
+        {images.map(i => (
+          <View key={i}>
+            <Image style={styles.image} source={i} />
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   summary: {
-    paddingTop: 10,
-    paddingBottom: 10,
+    paddingTop: 20,
     paddingLeft: 20,
     paddingRight: 20
+  },
+  imageReel: {
+    display: "flex",
+    flexDirection: "row"
+  },
+  image: {
+    width: 50,
+    height: 50
   }
 });
 
