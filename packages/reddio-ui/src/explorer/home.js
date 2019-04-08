f37cc0cb0554a607124ffa5430c9952129e041f1
Yanglin Zhao <yz1876@gmail.com>
2019-04-07 20:47:03 -0400

    Connect home to apollo client

--- packages/reddio-ui/src/explorer/home.js
+++ packages/reddio-ui/src/explorer/home.js
@@ -1,3 +1,63 @@
-import ListingResolver from "../listing/listing";
+import React from "react";
+import { View, Text, ActivityIndicator } from "react-native";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
 
-export default ListingResolver;
+const HOME_QUERY = gql`
+  query HomeQuery {
+    topSubreddits {
+      listings {
+        pathname
+        customInfo {
+          description
+        }
+        posts(limit: 10) {
+          name
+          thumbnail
+        }
+      }
+    }
+  }
+`;
+
+function ListingLoading() {
+  return (
+    <View>
+      <ActivityIndicator />
+    </View>
+  );
+}
+
+function ListingError() {
+  return (
+    <View>
+      <Text>:(</Text>
+    </View>
+  );
+}
+
+class Home extends React.Component {
+  render() {
+    return (
+      <Query query={HOME_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) {
+            return <ListingLoading />;
+          }
+
+          if (error) {
+            return <ListingError />;
+          }
+
+          return (
+            <View>
+              <Text>Hello world</Text>
+            </View>
+          );
+        }}
+      </Query>
+    );
+  }
+}
+
+export default Home;
