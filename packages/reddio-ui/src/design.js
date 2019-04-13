40133fe1056621b5a679da968be8cc6f7270706e
Yanglin Zhao <yz1876@gmail.com>
2019-04-13 00:25:00 -0400

    Create design.js

---
+++ packages/reddio-ui/src/design.js
@@ -0,0 +1,20 @@
+export const spacing = {
+  tiny: 5,
+  small: 10,
+  base: 15,
+  large: 20,
+  giant: 30
+};
+
+export const fontSize = {
+  tiny: 9,
+  small: 12,
+  base: 16,
+  large: 21,
+  larger1: 28,
+  larger2: 37,
+  larger3: 50
+};
+
+// TODO
+export const colors = {};
