3b9b478c0315798245d877e7ad531305f86e845d
Yanglin Zhao <yz1876@gmail.com>
2019-04-14 08:33:11 -0400

    Goodbye clojure, for now :(

--- src/reddio_frontend/components/loading_indicator/core.cljs
+++
@@ -1,6 +0,0 @@
-(ns reddio-frontend.components.loading-indicator.core
-  (:require))
-
-(defn loading-indicator []
-  [:div.loading-indicator
-   [:h2 "Loading"]])
--- test/reddio_frontend/lib/core_test.cljs
+++
@@ -1,7 +0,0 @@
-(ns reddio-frontend.lib.core-test
-  (:require [cljs.test :refer-macros [async deftest is testing]]
-            [reddio-frontend.lib.core :as core]))
-
-(deftest next-el-test
-  (is (= (core/next-el [1 2 3] 2) 3))
-  (is (nil? (core/next-el [1] 1))))
