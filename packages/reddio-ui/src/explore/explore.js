b0fe18b37f881678c50ad0d77b0b311a8cdcfacd
Yanglin Zhao <yz1876@gmail.com>
2019-04-17 21:11:13 -0400

    Create explore route

---
+++ packages/reddio-ui/src/explore/explore.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function Explore() {
+  return (
+    <View>
+      <Text>Hello world!</Text>
+    </View>
+  );
+}
+
+export default Explore;
