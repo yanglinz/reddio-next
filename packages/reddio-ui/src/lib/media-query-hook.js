274c2060375ff4e76e9b2c80b42dee18ba5a2cd1
Yanglin Zhao <yz1876@gmail.com>
2019-04-21 18:36:30 -0400

    Add layout width

--- packages/reddio-ui/src/lib/media-query-hook.js
+++ packages/reddio-ui/src/lib/media-query-hook.js
@@ -1,9 +1,15 @@
 import { useMedia } from "use-media";
 
+import * as design from "../design";
+
+const smallBreak = design.layoutWidth.small;
+const mediumBreak = design.layoutWidth.medium;
+const largeBreak = design.layoutWidth.large;
+
 function useMediaQuery() {
-  const large = useMedia({ minWidth: 1000 });
-  const medium = useMedia({ minWidth: 600 });
-  const small = useMedia({ minWidth: 400 });
+  const small = useMedia({ minWidth: smallBreak });
+  const medium = useMedia({ minWidth: mediumBreak });
+  const large = useMedia({ minWidth: largeBreak });
   return { large, medium, small };
 }
 
