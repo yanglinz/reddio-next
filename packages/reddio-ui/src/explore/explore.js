15ac309cc036acde600911c2bbdc25a8eaef83e9
Yanglin Zhao <yz1876@gmail.com>
2019-04-21 18:18:34 -0400

    Create listing styles

--- packages/reddio-ui/src/explore/explore.js
+++ packages/reddio-ui/src/explore/explore.js
@@ -1,15 +1,31 @@
 import React from "react";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 
 import Summary from "./summary";
 import * as data from "./data";
+import * as design from "../design";
 
 function Explore() {
   return (
-    <View>
-      <Summary title="Classical" listings={data.byGenre.classical} />
+    <View style={styles.explore}>
+      <View style={styles.exploreBackground}>
+        <Summary title="Classical" listings={data.byGenre.classical} />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  explore: {
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  exploreBackground: {
+    backgroundColor: "#fff",
+    marginTop: design.spacing.base,
+    marginBottom: design.spacing.base,
+    width: 550
+  }
+});
 
 export default Explore;
--- packages/reddio-ui/src/explore/summary.js
+++ packages/reddio-ui/src/explore/summary.js
@@ -1,19 +1,39 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+
+import * as design from "../design";
 
 function Summary(props) {
   const { title, listings } = props;
 
   return (
-    <View>
-      <Text>{title}</Text>
+    <View style={styles.summary}>
+      <Text style={styles.title}>{title}</Text>
       {listings.map(listing => (
-        <View key={listing}>
+        <View key={listing} style={styles.listing}>
           <Text>{listing}</Text>
         </View>
       ))}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  summary: {
+    padding: design.spacing.base
+  },
+  title: {
+    fontSize: design.spacing.base,
+    fontWeight: 900
+  },
+  listing: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    borderColor: "#ddd",
+    borderTopWidth: 1,
+    padding: design.spacing.small
+  }
+});
 
 export default Summary;
