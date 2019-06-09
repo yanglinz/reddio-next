8b7ce6aaac536f01c4fc42e9848890ab2277aec8
Yanglin Zhao <yz1876@gmail.com>
2019-06-09 11:58:50 -0400

    Add styled listing error screen

---
+++ packages/reddio-ui/src/listing/listing-error.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+import * as design from "../design";
+
+function ListingError() {
+  return (
+    <View style={styles.error}>
+      <Text style={styles.icon}>:(</Text>
+      <Text style={styles.title}>Oops, something went wrong.</Text>
+      <Text style={styles.subtitle}>
+        Looks like we couldn't load your content
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  error: {
+    paddingTop: design.spacing.base,
+    paddingBottom: design.spacing.large
+  },
+  icon: {
+    color: design.colors.primary.c6,
+    fontSize: 240,
+    textAlign: "center"
+  },
+  title: {
+    color: design.colors.neutral.c4,
+    fontSize: design.fontSize.large,
+    marginTop: design.spacing.large,
+    textAlign: "center"
+  },
+  subtitle: {
+    color: design.colors.neutral.c7,
+    fontSize: design.fontSize.large,
+    marginTop: design.spacing.small,
+    textAlign: "center"
+  }
+});
+
+export default ListingError;
