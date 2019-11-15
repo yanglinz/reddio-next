f57cb4abd8f5e51cdeb6ec7cd027ef6a104ce57b
Yanglin Zhao <yz1876@gmail.com>
2019-11-15 17:03:56 -0500

    Fix storybook

--- .storybook/decorators.js
+++ .storybook/decorators.js
@@ -1,15 +1,16 @@
 import React from "react";
-import { StyleSheet, View } from "react-native-web";
-
-const styles = StyleSheet.create({
-  root: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    minHeight: "100vh"
-  }
-});
 
 export function centeredDecorator(renderStory) {
-  return <View style={styles.root}>{renderStory()}</View>;
+  return React.createElement(
+    "div",
+    {
+      style: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh"
+      }
+    },
+    renderStory()
+  );
 }
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,15 +1,11 @@
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-        loader: "file-loader"
-      }
-    ]
-  },
-  resolve: {
-    alias: {
-      "react-native$": "react-native-web"
-    }
-  }
+module.exports = async ({ config, mode }) => {
+  config.module.rules.push({
+    test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+    loader: "file-loader"
+  });
+
+  config.resolve.alias = config.resolve.alias || {};
+  config.resolve.alias["react-native$"] = "react-native-web";
+
+  return config;
 };
