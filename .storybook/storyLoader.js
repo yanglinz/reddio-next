c0063d99ba7b88004d1f70bedc6cef29ca4ad5de
Yanglin Zhao <yz1876@gmail.com>
2019-11-15 16:14:17 -0500

    Update development dependencies

--- .storybook/storyLoader.js
+++ .storybook/storyLoader.js
@@ -1,27 +1,24 @@
-
 // Auto-generated file created by react-native-storybook-loader
 // Do not edit.
 //
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
-  require('../src/brand/header.story');
-  require('../src/home/listing-summary.story');
-  require('../src/lib/skeleton.story');
-  require('../src/lib/thumbnail.story');
-  require('../src/listing/listing-header.story');
-  require('../src/listing/post-list.story');
-  
+	require('../src/brand/header.story');
+	require('../src/home/listing-summary.story');
+	require('../src/lib/skeleton.story');
+	require('../src/lib/thumbnail.story');
+	require('../src/listing/listing-header.story');
+	require('../src/listing/post-list.story');
 }
 
 const stories = [
-  '../src/brand/header.story',
-  '../src/home/listing-summary.story',
-  '../src/lib/skeleton.story',
-  '../src/lib/thumbnail.story',
-  '../src/listing/listing-header.story',
-  '../src/listing/post-list.story',
-  
+	'../src/brand/header.story',
+	'../src/home/listing-summary.story',
+	'../src/lib/skeleton.story',
+	'../src/lib/thumbnail.story',
+	'../src/listing/listing-header.story',
+	'../src/listing/post-list.story'
 ];
 
 module.exports = {
