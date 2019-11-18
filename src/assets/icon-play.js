92a8671ecea5287f191d9bc9d7ed9e2d60352e52
Yanglin Zhao <yz1876@gmail.com>
2019-11-18 10:55:06 -0500

    Fix path padding

--- src/assets/icon-play.js
+++ src/assets/icon-play.js
@@ -2,16 +2,16 @@ import React from "react";
 
 function IconPlay(props) {
   const color = props.color || "#222";
-  const defaultSize = 24;
+  const defaultSize = 20;
   const size = props.size || defaultSize;
   const scale = props.size / defaultSize;
 
   return (
     <svg width={size} height={size} viewBox={`0 0 ${size} ${size}`}>
       <g transform={`scale(${scale})`}>
-        <path d="M0 0h24v24H0z" fill="none" />
+        <path d="M-2-2h24v24H-2z" fill="none" />
         <path
-          d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z"
+          d="M10 0C4.48 0 0 4.48 0 10s4.48 10 10 10 10-4.48 10-10S15.52 0 10 0zM8 14.5v-9l6 4.5z"
           fill={color}
         />
       </g>
