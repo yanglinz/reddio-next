00dddc8780a45805db58b9146ed201469a180691
Yanglin Zhao <yz1876@gmail.com>
2019-11-18 20:32:04 -0500

    Remove width from storybook

---
+++ src/assets/icon-skip-prev.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function IconSkipPrev(props) {
+  const color = props.color || "#222";
+  const defaultSize = 12;
+  const size = props.size || defaultSize;
+  const scale = props.size / defaultSize;
+
+  return (
+    <svg width={size} height={size} viewBox={`0 0 ${size} ${size}`}>
+      <g transform={`scale(${scale})`}>
+        <path d="M0 0h2v12H0zm3.5 6l8.5 6V0z" fill={color} />
+        <path d="M-6-6h24v24H-6z" fill="none" />
+      </g>
+    </svg>
+  );
+}
+
+export default IconSkipPrev;
--- src/assets/icons.story.js
+++ src/assets/icons.story.js
@@ -4,19 +4,35 @@ import { storiesOf } from "@storybook/react";
 
 import IconPause from "./icon-pause";
 import IconPlay from "./icon-play";
+import IconSkipNext from "./icon-skip-next";
+import IconSkipPrev from "./icon-skip-prev";
 
 storiesOf("Icons", module)
   .add("play icon", () => (
-    <View style={{ width: 300 }}>
+    <View>
       <IconPlay size={24} />
       <IconPlay size={150} />
       <IconPlay size={250} />
     </View>
   ))
   .add("pause icon", () => (
-    <View style={{ width: 300 }}>
+    <View>
       <IconPause size={24} />
       <IconPause size={150} />
       <IconPause size={250} />
+    </View>
+  ))
+  .add("skip next icon", () => (
+    <View>
+      <IconSkipNext size={24} />
+      <IconSkipNext size={150} />
+      <IconSkipNext size={250} />
+    </View>
+  ))
+  .add("skip prev icon", () => (
+    <View>
+      <IconSkipPrev size={24} />
+      <IconSkipPrev size={150} />
+      <IconSkipPrev size={250} />
     </View>
   ));
