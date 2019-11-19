66435be5ddc97d49997f18828dc22f08b7937467
Yanglin Zhao <yanglinz@users.noreply.github.com>
2019-11-18 21:21:30 -0500

    Add animated play pause button (#61)
    * Add play and pause buttons
    
    * Create outline of play-pause component
    
    * Fix path padding
    
    * Fix path for pause
    
    * Remove width from storybook
    
    * Style play pause

--- .storybook/storyLoader.js
+++ .storybook/storyLoader.js
@@ -4,21 +4,25 @@
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
+	require('../src/assets/icons.story');
 	require('../src/brand/header.story');
 	require('../src/home/listing-summary.story');
 	require('../src/lib/skeleton.story');
 	require('../src/lib/thumbnail.story');
 	require('../src/listing/listing-header.story');
 	require('../src/listing/post-list.story');
+	require('../src/player/play-pause.story');
 }
 
 const stories = [
+	'../src/assets/icons.story',
 	'../src/brand/header.story',
 	'../src/home/listing-summary.story',
 	'../src/lib/skeleton.story',
 	'../src/lib/thumbnail.story',
 	'../src/listing/listing-header.story',
-	'../src/listing/post-list.story'
+	'../src/listing/post-list.story',
+	'../src/player/play-pause.story'
 ];
 
 module.exports = {
---
+++ src/assets/icon-pause.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function IconPause(props) {
+  const color = props.color || "#222";
+  const defaultSize = 20;
+  const size = props.size || defaultSize;
+  const scale = props.size / defaultSize;
+
+  return (
+    <svg width={size} height={size} viewBox={`0 0 ${size} ${size}`}>
+      <g transform={`scale(${scale})`}>
+        <path d="M-2-2h24v24H-2z" fill="none" />
+        <path
+          d="M10 0C4.48 0 0 4.48 0 10s4.48 10 10 10 10-4.48 10-10S15.52 0 10 0zM9 14H7V6h2zm4 0h-2V6h2z"
+          fill={color}
+        />
+      </g>
+    </svg>
+  );
+}
+
+export default IconPause;
