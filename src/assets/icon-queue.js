c6d0d986f8de689f65b16ee258ef1143ba4e4c57
Yanglin Zhao <yz1876@gmail.com>
2019-11-22 22:37:12 -0500

    Add the rest of the icons

---
+++ src/assets/icon-queue.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function IconQueue(props) {
+  const color = props.color || "#222";
+  const defaultSize = 19;
+  const size = props.size || defaultSize;
+  const scale = props.size / defaultSize;
+  const width = size;
+  const height = width * (14 / 19);
+
+  return (
+    <svg width={width} height={height} viewBox={`0 0 ${width} ${height}`}>
+      <g transform={`scale(${scale})`}>
+        <path d="M-3-6h24v24H-3z" fill="none" />
+        <path
+          d="M12 0H0v2h12zm0 4H0v2h12zM0 10h8V8H0zM14 0v8.18c-.31-.11-.65-.18-1-.18-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3V2h3V0z"
+          fill={color}
+        />
+      </g>
+    </svg>
+  );
+}
+
+export default IconQueue;
---
+++ src/assets/icon-replay.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function IconReplay(props) {
+  const color = props.color || "#222";
+  const defaultSize = 20;
+  const size = props.size || defaultSize;
+  const scale = props.size / defaultSize;
+
+  return (
+    <svg width={size} height={size} viewBox={`0 0 ${size} ${size}`}>
+      <g transform={`scale(${scale})`}>
+        <path d="M-4-1h24v24H-4z" fill="none" />
+        <path
+          d="M8 4V0L3 5l5 5V6c3.31 0 6 2.69 6 6s-2.69 6-6 6-6-2.69-6-6H0c0 4.42 3.58 8 8 8s8-3.58 8-8-3.58-8-8-8z"
+          fill={color}
+        />
+      </g>
+    </svg>
+  );
+}
+
+export default IconReplay;
