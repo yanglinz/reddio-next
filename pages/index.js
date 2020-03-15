6ef5947e225c994f52755e9577058ba7102b39ca
Yanglin Zhao <yz1876@gmail.com>
2020-03-15 00:47:07 -0400

    Add header to index

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-import Home from "../src/screens/Home/Home";
+import Header from "../src/screens/Header";
 import AppWrapper from "../src/screens/AppWrapper";
+import Home from "../src/screens/Home/Home";
 
 function HomeRoute() {
   return (
     <AppWrapper>
+      <Header />
       <Home />
     </AppWrapper>
   );
