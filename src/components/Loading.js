d68aa0298eaa68e45275a87b4f214607021f342f
Yanglin Zhao <yz1876@gmail.com>
2020-03-28 16:37:18 -0400

    Remove the react-native-web view from loading component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,21 +1,10 @@
 import React from "react";
-import { ActivityIndicator, View, StyleSheet } from "react-native-web";
+import { ActivityIndicator } from "react-native-web";
 
 import * as design from "../design";
 
 function Loading() {
-  return (
-    <View style={styles.loading}>
-      <ActivityIndicator size="large" color={design.colors.primary.c4} />
-    </View>
-  );
+  return <ActivityIndicator size="large" color={design.colors.primary.c4} />;
 }
-
-const styles = StyleSheet.create({
-  loading: {
-    marginTop: design.spacing.large,
-    marginBottom: design.spacing.large
-  }
-});
 
 export default Loading;
