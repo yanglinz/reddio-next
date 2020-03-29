f5e2aa1cf7f7c87f63fb169e8a774e69a97f8d38
Yanglin Zhao <yz1876@gmail.com>
2020-03-29 12:11:38 -0400

    Convert explore component to css modules

--- src/screens/Explore/Explore.js
+++ src/screens/Explore/Explore.js
@@ -1,46 +1,22 @@
 import React from "react";
-import { View, StyleSheet } from "react-native-web";
 
 import Summary from "./Summary";
 import * as Layout from "../../components/Layout";
-import * as design from "../../design";
 import * as data from "./data";
 
-function Explore() {
-  // TODO fix media query
-  const isMediumWidth = true;
-  const style = isMediumWidth
-    ? [styles.exploreBg, styles.exploreBgMed]
-    : styles.exploreBg;
+import styles from "./Explore.module.scss";
 
+function Explore() {
   return (
-    <View style={styles.explore}>
-      <Layout.Standard>
-        <View style={style}>
-          <Summary title="Electronic" listings={data.byGenre.electronic} />
-          <Summary title="Rock / Alternative" listings={data.byGenre.rock} />
-          <Summary title="Hip-Hop" listings={data.byGenre.hiphop} />
-          <Summary title="Classical" listings={data.byGenre.classical} />
-        </View>
-      </Layout.Standard>
-    </View>
+    <Layout.Standard>
+      <div className={styles.Explore}>
+        <Summary title="Electronic" listings={data.byGenre.electronic} />
+        <Summary title="Rock / Alternative" listings={data.byGenre.rock} />
+        <Summary title="Hip-Hop" listings={data.byGenre.hiphop} />
+        <Summary title="Classical" listings={data.byGenre.classical} />
+      </div>
+    </Layout.Standard>
   );
 }
-
-const styles = StyleSheet.create({
-  explore: {
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  exploreBg: {
-    backgroundColor: "#fff",
-    marginTop: design.spacing.base,
-    marginBottom: design.spacing.larger2
-  },
-  exploreBgMed: {
-    marginTop: design.spacing.base,
-    marginBottom: design.spacing.larger2
-  }
-});
 
 export default Explore;
