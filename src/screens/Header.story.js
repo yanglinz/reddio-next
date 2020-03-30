e1bc1f201b56c9319dc3944aaeac5d47b6893e02
Yanglin Zhao <yz1876@gmail.com>
2020-03-30 19:22:58 -0400

    Remove view from header story

--- src/screens/Header.story.js
+++ src/screens/Header.story.js
@@ -1,11 +1,10 @@
 import React from "react";
-import { View } from "react-native-web";
 import { storiesOf } from "@storybook/react";
 
 import { Header } from "./Header";
 
 storiesOf("Header", module).add("default", () => (
-  <View style={{ width: 680 }}>
+  <div style={{ width: 680 }}>
     <Header />
-  </View>
+  </div>
 ));
