3b16223a6c6fc5bde911addc40efcbf8bf2e0c8d
Yanglin Zhao <yz1876@gmail.com>
2020-04-04 20:28:29 -0400

    Shuffle some components in screens

--- pages/[...resolver].js
+++ pages/[...resolver].js
@@ -1,5 +1,5 @@
-import Header from "../src/screens/Header";
-import AppWrapper from "../src/screens/AppWrapper";
+import Header from "../src/screens/App/Header";
+import AppWrapper from "../src/screens/App/AppWrapper";
 import ListingResolver from "../src/screens/Listing/Listing";
 
 function ResolverRoute() {
