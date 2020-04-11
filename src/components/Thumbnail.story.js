2248ba09d49a893706250782ea76e47b0b16404b
Yanglin Zhao <yz1876@gmail.com>
2020-04-11 17:21:36 -0400

    Add more stories for thumbnail

--- src/components/Thumbnail.story.js
+++ src/components/Thumbnail.story.js
@@ -3,22 +3,58 @@ import { storiesOf } from "@storybook/react";
 
 import Thumbnail from "./Thumbnail";
 
-storiesOf("Thumbnail", module).add("default", () => (
-  <div style={{ width: 100 }}>
+storiesOf("Thumbnail", module)
+  .add("default", () => (
     <Thumbnail
-      title="Example thumbnail"
-      width={50}
-      height={50}
+      title="Thumbnail"
+      width={75}
+      height={75}
       seed="some-seed"
       src="https://picsum.photos/id/42/200/300"
     />
-
+  ))
+  .add("fallback", () => (
     <Thumbnail
-      title="Example thumbnail"
-      width={50}
-      height={50}
+      title="Thumbnail"
+      width={75}
+      height={75}
       seed="another-seed"
       src="http://bad-image.com/test.jpg"
     />
-  </div>
-));
+  ))
+  .add("large", () => (
+    <Thumbnail
+      title="Thumbnail"
+      width={150}
+      height={150}
+      seed="another-seed"
+      src="https://picsum.photos/id/42/200/300"
+    />
+  ))
+  .add("large fallback", () => (
+    <Thumbnail
+      title="Thumbnail"
+      width={150}
+      height={150}
+      seed="another-seed"
+      src="http://bad-image.com/test.jpg"
+    />
+  ))
+  .add("stacked", () => (
+    <div style={{ width: 75 }}>
+      <Thumbnail
+        title="Thumbnail"
+        width={75}
+        height={75}
+        seed="some-seed"
+        src="https://picsum.photos/id/42/200/300"
+      />
+      <Thumbnail
+        title="Example thumbnail"
+        width={75}
+        height={75}
+        seed="another-seed"
+        src="http://bad-image.com/test.jpg"
+      />
+    </div>
+  ));
