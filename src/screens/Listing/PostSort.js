e0917a58a7895cbfe61340f7ad6c8ee066eee736
Yanglin Zhao <yz1876@gmail.com>
2020-05-06 00:29:39 -0400

    Remove react-native-web from PostSort

--- src/screens/Listing/PostSort.js
+++ src/screens/Listing/PostSort.js
@@ -1,65 +1,70 @@
 import React from "react";
-import { View, StyleSheet } from "react-native-web";
 import Router from "next/router";
 import { Listbox, ListboxOption } from "@reach/listbox";
 
+import { Box, Inline } from "../../vendor/ui-system";
 import * as reddit from "../../lib/reddit";
-import * as design from "../../styles/design";
+import { space as s } from "../../styles/design";
 
 export function PostListSort(props) {
   const { pathname } = props;
   const currentSortType = reddit.getSortType(pathname);
 
   return (
-    <View style={styles.postListSort}>
-      <Listbox
-        value={currentSortType}
-        onChange={sortType => {
-          const nextPath = reddit.resolveSortTypePath(pathname, sortType);
-          Router.replace(nextPath);
-        }}
-      >
-        <ListboxOption value={reddit.sortTypes.hot}>Hot</ListboxOption>
-        <ListboxOption value={reddit.sortTypes.new}>New</ListboxOption>
-        <ListboxOption value={reddit.sortTypes.top}>Top</ListboxOption>
-        <ListboxOption value={reddit.sortTypes.controversial}>
-          Controversial
-        </ListboxOption>
-        <ListboxOption value={reddit.sortTypes.rising}>Rising</ListboxOption>
-      </Listbox>
-
-      {currentSortType === reddit.sortTypes.top ? (
-        <View style={styles.sortRange}>
+    <div>
+      <Box padding={s.m}>
+        <Inline spacing={s.s}>
           <Listbox
-            value={reddit.getSortRange(pathname)}
-            onChange={sortRange => {
-              const nextPath = reddit.resolveSortRangePath(pathname, sortRange);
+            value={currentSortType}
+            onChange={sortType => {
+              const nextPath = reddit.resolveSortTypePath(pathname, sortType);
               Router.replace(nextPath);
             }}
           >
-            <ListboxOption value={reddit.sortRanges.hour}>Hour</ListboxOption>
-            <ListboxOption value={reddit.sortRanges.day}>Day</ListboxOption>
-            <ListboxOption value={reddit.sortRanges.week}>Week</ListboxOption>
-            <ListboxOption value={reddit.sortRanges.month}>Month</ListboxOption>
-            <ListboxOption value={reddit.sortRanges.year}>Year</ListboxOption>
-            <ListboxOption value={reddit.sortRanges.all}>All</ListboxOption>
+            <ListboxOption value={reddit.sortTypes.hot}>Hot</ListboxOption>
+            <ListboxOption value={reddit.sortTypes.new}>New</ListboxOption>
+            <ListboxOption value={reddit.sortTypes.top}>Top</ListboxOption>
+            <ListboxOption value={reddit.sortTypes.controversial}>
+              Controversial
+            </ListboxOption>
+            <ListboxOption value={reddit.sortTypes.rising}>
+              Rising
+            </ListboxOption>
           </Listbox>
-        </View>
-      ) : null}
-    </View>
+
+          {currentSortType === reddit.sortTypes.top ? (
+            <div>
+              <Listbox
+                value={reddit.getSortRange(pathname)}
+                onChange={sortRange => {
+                  const nextPath = reddit.resolveSortRangePath(
+                    pathname,
+                    sortRange
+                  );
+                  Router.replace(nextPath);
+                }}
+              >
+                <ListboxOption value={reddit.sortRanges.hour}>
+                  Hour
+                </ListboxOption>
+                <ListboxOption value={reddit.sortRanges.day}>Day</ListboxOption>
+                <ListboxOption value={reddit.sortRanges.week}>
+                  Week
+                </ListboxOption>
+                <ListboxOption value={reddit.sortRanges.month}>
+                  Month
+                </ListboxOption>
+                <ListboxOption value={reddit.sortRanges.year}>
+                  Year
+                </ListboxOption>
+                <ListboxOption value={reddit.sortRanges.all}>All</ListboxOption>
+              </Listbox>
+            </div>
+          ) : null}
+        </Inline>
+      </Box>
+    </div>
   );
 }
-
-const styles = StyleSheet.create({
-  postListSort: {
-    paddingTop: design.spacing.base,
-    paddingBottom: design.spacing.base,
-    paddingLeft: design.spacing.small,
-    paddingRight: design.spacing.small
-  },
-  sortRange: {
-    paddingTop: design.spacing.smaller2
-  }
-});
 
 export default PostListSort;
