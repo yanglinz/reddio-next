0e05785af4effbaa2e350310079e45ea1c90c6b8
Yanglin Zhao <yz1876@gmail.com>
2020-05-09 22:05:37 -0400

    Use new layout components in Header

--- src/screens/App/Header.js
+++ src/screens/App/Header.js
@@ -1,23 +1,23 @@
 import React from "react";
 import Link from "next/link";
 
+import { Box, Inline, Stack, Text } from "../../vendor/ui-system";
 import LogoAnimation from "../../components/LogoAnimation";
-import { Box, Inline, Stack } from "../../components/Spacing";
-import * as Text from "../../components/Text";
 import * as Layout from "../../components/Layout";
+import * as design from "../../styles/design";
 
 import styles from "./Header.module.scss";
 
 function HeaderNav(props) {
   const { fullSize } = props;
   return (
-    <Box spacing="m">
+    <Box padding={design.space.m}>
       <Link href="/">
         <a className={styles.HeaderNavBrand}>
           {fullSize ? (
             <LogoAnimation />
           ) : (
-            <Inline spacing="s">
+            <Inline spacing={design.space.s} vcentered>
               <LogoAnimation />
               <span className={styles.HeaderNavDivider}></span>
               <span className={styles.HeaderNavTitle}>Reddio</span>
@@ -33,16 +33,16 @@ function HeaderIntro() {
   return (
     <div className={styles.Intro}>
       <Layout.Wide>
-        <Stack spacing="xl">
-          <Text.Heading2 className={styles.IntroSiteTitle} size="xxxl">
+        <Stack spacing={design.space.xl}>
+          <Text className={styles.IntroSiteTitle} size={67}>
             Reddio
-          </Text.Heading2>
-          <Text.Heading2 className={styles.IntroTitle} size="xxl">
+          </Text>
+          <Text className={styles.IntroTitle} size={42}>
             Discover and listen to music.
-          </Text.Heading2>
-          <Text.Heading3 className={styles.IntroSubtitle} size="xl">
+          </Text>
+          <Text className={styles.IntroSubtitle} size={26}>
             Powered by hand curated content from reddit.
-          </Text.Heading3>
+          </Text>
         </Stack>
       </Layout.Wide>
     </div>
